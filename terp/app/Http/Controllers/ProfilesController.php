d75bef65879fdeadf6695a35fd5dc84c8f768db9
jessfatal <jessfatal1@gmail.com>
2020-09-02 10:02:16 -0400

    small changes

--- terp/app/Http/Controllers/ProfilesController.php
+++ terp/app/Http/Controllers/ProfilesController.php
@@ -59,7 +59,7 @@ class ProfilesController extends Controller
         $imagePath = null;
 
         if (request()->hasFile('image')) {
-            $imagePath = request('image')->store('uploads', 'public');
+            $imagePath = request('image')->getRealPath()->store('uploads', 'public');
 
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
             $image->save();
